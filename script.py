24812b6998d2ef9bcbc34be7d3585621fc5ee7d2
maxn-csi <maksimnaydin@coherentsolutions.com>
2020-06-01 16:13:13 +0300

    Working version

--- script.py
+++ script.py
@@ -2,43 +2,23 @@
 # each.[commit_id, commit_date, commit_message, repository]
 
 import csv, json, sys
-from pandas import json_normalize
+import pandas as pd
 
 fileInput = sys.argv[1]
 fileOutput = sys.argv[2]
-inputFile = open(fileInput)
-outputFile = open(fileOutput, 'w')
+# fileInput = 'report.json'
+# fileOutput = 'report.csv'
 
-def flatten_json(y):
-    out = {}
+with open(fileInput) as f:
+    data = json.load(f)
 
-    def flatten(x, name=''):
-        if type(x) is dict:
-            for a in x:
-                flatten(x[a], name + a + '_')
-        elif type(x) is list:
-            i = 0
-            for a in x:
-                flatten(a, name + str(i) + '_')
-                i += 1
-        else:
-            out[name[:-1]] = x
+dataNorm = pd.json_normalize(data)              # read and normalize data
 
-    flatten(y)
-    return out
+dataNormExp = pd.json_normalize(data=data, record_path='sub_issues', 
+                            meta=['issue_key', 'fix_version'])      # expand 'sub_issues'
 
-data = json.load(inputFile)
+columns_titles = ["fix_version", "issue_key", "sub_issue_key", "commit_id", "commit_date",
+                    "commit_message", "repository"]
+dataReindex=dataNormExp.reindex(columns=columns_titles)             # sorting columns
 
-print(data)
-print(json_normalize(data))
-print(json_normalize(flatten_json(data)))
-
-inputFile.close()
-
-output = csv.writer(outputFile)
-
-output.writerow(data[0].keys())  # header row
-for row in data:
-    output.writerow(row.values()) #values row
-# for row in (data["sub_issues"]):
-#     output.writerow(row.values()) #values row
+dataReindex.to_csv(fileOutput, index=False)      # write csv
